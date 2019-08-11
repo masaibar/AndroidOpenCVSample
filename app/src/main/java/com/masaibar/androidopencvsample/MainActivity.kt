6cbf4990a16275a54b19f2a3a72fa3a5caf47798
masaibar <masaibar.dev@gmail.com>
2019-08-11 23:35:28 +0900

    初期化してビルドが通るところまで

--- app/src/main/java/com/masaibar/androidopencvsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/androidopencvsample/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.masaibar.androidopencvsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        System.loadLibrary("opencv_java4")
         setContentView(R.layout.activity_main)
+        OpenCVLoader.initDebug()
     }
 }
