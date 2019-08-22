31b765603afdf42e7a52e0917f0ee1f018c73e92
masaibar <masaibar.dev@gmail.com>
2019-08-22 21:17:13 +0900

    モザイク処理

--- app/src/main/java/com/masaibar/androidopencvsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/androidopencvsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.masaibar.androidopencvsample
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,44 +10,15 @@ import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.Mat
-import org.opencv.core.MatOfRect
-import org.opencv.core.Point
-import org.opencv.core.Scalar
+import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
-import org.opencv.objdetect.CascadeClassifier
-import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private const val FILE_NAME = "haarcascade_frontalface_alt.xml"
-    }
-
     private val onLoaderCallback = object : BaseLoaderCallback(this) {
         override fun onManagerConnected(status: Int) {
             super.onManagerConnected(status)
-
-            val file = File(filesDir.path + File.separator + FILE_NAME)
-            if (!file.exists()) {
-                try {
-                    val inputStream = assets.open(FILE_NAME)
-                    val fileOutputStream = FileOutputStream(file, false)
-                    val buffer = ByteArray(1024)
-                    var read: Int
-                    do {
-                        read = inputStream.read(buffer)
-                        if (read == -1) {
-                            break
-                        }
-                        fileOutputStream.write(buffer, 0, read)
-                    } while (true)
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-            val cascadeClassifier = CascadeClassifier(file.absolutePath)
 
             try {
                 val inputStream = assets.open("okawa_reiwa.jpg")
@@ -54,21 +26,37 @@ class MainActivity : AppCompatActivity() {
                 val mat = Mat()
                 Utils.bitmapToMat(bitmap, mat)
 
-                val matOfRect = MatOfRect()
-                cascadeClassifier.detectMultiScale(mat, matOfRect)
+                Imgproc.resize(
+                    mat,
+                    mat,
+                    Size(
+                        0.0,
+                        0.0
+                    ),
+                    0.1,
+                    0.1,
+                    Imgproc.INTER_AREA
+                )
+                Imgproc.resize(
+                    mat,
+                    mat,
+                    Size(
+                        0.0,
+                        0.0
+                    ),
+                    10.0,
+                    10.0,
+                    Imgproc.INTER_AREA
+                )
 
-                matOfRect.toArray().forEach { rect ->
-                    Imgproc.rectangle(
-                        mat,
-                        Point(rect.x.toDouble(), rect.y.toDouble()),
-                        Point(rect.x + rect.width.toDouble(), rect.y + rect.height.toDouble()),
-                        Scalar(0.0, 255.0, 0.0),
-                        5
-                    )
-                }
-                Utils.matToBitmap(mat, bitmap)
+                val dstBitmap = Bitmap.createBitmap(
+                    mat.size().width.toInt(),
+                    mat.size().height.toInt(),
+                    Bitmap.Config.ARGB_8888
+                )
+                Utils.matToBitmap(mat, dstBitmap)
 
-                image.setImageBitmap(bitmap)
+                image.setImageBitmap(dstBitmap)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
@@ -79,7 +67,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(OpenCVLoader.initDebug()) {
+        if (OpenCVLoader.initDebug()) {
             System.loadLibrary("opencv_java4")
             onLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
         }
