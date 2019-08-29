92856cfdf2c2dc1144b080045288d4e20143839e
masaibar <masaibar.dev@gmail.com>
2019-08-29 22:42:48 +0900

    K-means++で減色処理

--- app/src/main/java/com/masaibar/androidopencvsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/androidopencvsample/MainActivity.kt
@@ -9,12 +9,14 @@ import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
-import org.opencv.core.Mat
-import org.opencv.core.Size
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val K = 10
+    }
 
     private val onLoaderCallback = object : BaseLoaderCallback(this) {
         override fun onManagerConnected(status: Int) {
@@ -23,40 +25,77 @@ class MainActivity : AppCompatActivity() {
             try {
                 val inputStream = assets.open("okawa_reiwa.jpg")
                 val bitmap = BitmapFactory.decodeStream(inputStream)
-                val mat = Mat()
-                Utils.bitmapToMat(bitmap, mat)
+                val cutout = Mat()
+                Utils.bitmapToMat(bitmap, cutout)
+                Imgproc.cvtColor(cutout, cutout, Imgproc.COLOR_BGRA2BGR)
 
-                Imgproc.resize(
-                    mat,
-                    mat,
-                    Size(
-                        0.0,
-                        0.0
-                    ),
-                    0.05,
-                    0.05,
-                    Imgproc.INTER_AREA
-                )
-                Imgproc.resize(
-                    mat,
-                    mat,
-                    Size(
-                        0.0,
-                        0.0
-                    ),
-                    20.0,
-                    20.0,
-                    Imgproc.INTER_AREA
-                )
+                val samples = cutout.reshape(1, cutout.cols() * cutout.rows())
+                val samples32f = Mat()
+                samples.convertTo(samples32f, CvType.CV_32F, 1.0 / 255.0)
 
+                val labels = Mat()
+                val criteria = TermCriteria(TermCriteria.COUNT, 100, 1.0)
+                val centers = Mat()
+                Core.kmeans(samples32f, K, labels, criteria, 1, Core.KMEANS_PP_CENTERS, centers)
+
+                centers.convertTo(centers, CvType.CV_8UC1, 255.0)
+                centers.reshape(3)
+
+                var dst = cutout.clone()
+                var rows = 0
+                var y = 0
+                while (y < cutout.rows()) {
+                    var x = 0
+                    while (x < cutout.cols()) {
+                        val label = labels.get(rows, 0)[0].toInt()
+                        val r = centers.get(label, 2)[0]
+                        val g = centers.get(label, 1)[0]
+                        val b = centers.get(label, 0)[0]
+                        dst.put(y, x, b, g, r)
+                        rows += 1
+                        x += 1
+                    }
+                    y += 1
+                }
                 val dstBitmap = Bitmap.createBitmap(
-                    mat.size().width.toInt(),
-                    mat.size().height.toInt(),
+                    dst.size().width.toInt(),
+                    dst.size().height.toInt(),
                     Bitmap.Config.ARGB_8888
                 )
-                Utils.matToBitmap(mat, dstBitmap)
-
+                Utils.matToBitmap(dst, dstBitmap)
                 image.setImageBitmap(dstBitmap)
+
+//                Imgproc.resize(
+//                    mat,
+//                    mat,
+//                    Size(
+//                        0.0,
+//                        0.0
+//                    ),
+//                    0.05,
+//                    0.05,
+//                    Imgproc.INTER_AREA
+//                )
+//                Imgproc.resize(
+//                    mat,
+//                    mat,
+//                    Size(
+//                        0.0,
+//                        0.0
+//                    ),
+//                    20.0,
+//                    20.0,
+//                    Imgproc.INTER_AREA
+//                )
+//
+//                val dstBitmap = Bitmap.createBitmap(
+//                    mat.size().width.toInt(),
+//                    mat.size().height.toInt(),
+//                    Bitmap.Config.ARGB_8888
+//                )
+//                Utils.matToBitmap(mat, dstBitmap)
+//
+//                image.setImageBitmap(dstBitmap)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
